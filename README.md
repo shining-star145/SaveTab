aa83169f4214165f24149b16308e71b3ecfd63bc
shining-star145 <107754187+shining-star145@users.noreply.github.com>
2023-09-28 00:03:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# coc8640b4ab7ea074c56f6bdc
+
+1. Chrome Extension
+2. Used for saving a current tab by
+    2.1 By typing the url
+    2.2 Clicking on the Save Tab option -> Saves the current active window's url
+3. Tabs saved are arranged in random order and suffles each time you save a tab or reopen chrome 
+
---
+++ index.js
@@ -0,0 +1,62 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    randomize(myLeads)
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        randomize(myLeads)
+        render(myLeads)
+    })
+})
+
+function randomize(leads) {
+    // Fisher Yates Suffle Algorithm for randomizing the tabs saved ....
+    for(let i = leads.length-1; i>=1; i--){
+        let x = Math.random();
+        x *= (i);
+        x = Math.floor(x);
+        let temp1 = leads[x];
+        leads[x] = leads[i];
+        leads[i] = temp1;
+    }
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    randomize(myLeads)
+    render(myLeads)
+})
